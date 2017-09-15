70de027bfa2dacb03fdfcb301f0cb21532a69cfb
Adrian31 <adrian_carabetta@hotmail.com>
2017-09-15 12:34:17 +0800

    Created a function for the students

---
+++ js/app.js
@@ -0,0 +1,77 @@
+/* STUDENTS IGNORE THIS FUNCTION
+ * All this does is create an initial
+ * attendance record if one is not found
+ * within localStorage.
+ */
+(function() {
+    if (!localStorage.attendance) {
+        console.log('Creating attendance records...');
+        function getRandom() {
+            return (Math.random() >= 0.5);
+        }
+
+        var nameColumns = $('tbody .name-col'),
+            attendance = {};
+
+        nameColumns.each(function() {
+            var name = this.innerText;
+            attendance[name] = [];
+
+            for (var i = 0; i <= 11; i++) {
+                attendance[name].push(getRandom());
+            }
+        });
+
+        localStorage.attendance = JSON.stringify(attendance);
+    }
+}());
+
+/********** MODEL **********/
+var model: function {
+  days: 12,
+
+  student: [
+    {
+      name: 'Slappy the Frog'
+    },
+    {
+      name: 'Lilly the Lizard'
+    },
+    {
+      name: 'Paulrus the Walrus'
+    },
+    {
+      name: 'Gregory the Goat'
+    },
+    {
+      name: 'Adam the Anaconda'
+    }
+  ]
+};
+
+/********** OCTOPUS **********/
+
+/********** VIEW **********/
+function tableCreate() {
+    var body = document.getElementsByTagName('body')[0];
+    var tbl = document.createElement('table');
+    tbl.style.width = '100%';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+    for (var i = 0; i < 3; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < 2; j++) {
+            if (i == 2 && j == 1) {
+                break
+            } else {
+                var td = document.createElement('td');
+                td.appendChild(document.createTextNode('\u0020'))
+                i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+                tr.appendChild(td)
+            }
+        }
+        tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+    body.appendChild(tbl)
+}
