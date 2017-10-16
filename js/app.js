38eca9f645d2ca9203af82959e1fc65d708967d0
Adrian31 <adrian_carabetta@hotmail.com>
2017-10-16 14:26:34 +0800

    Added the last column with a new variable and added old code which needs to be added to mvo

--- js/app.js
+++ js/app.js
@@ -33,23 +33,28 @@ var model = {
   students: [
     {
       name: 'Slappy the Frog',
-      days: [1,2,3,4,5,6,7,8,9,10,11,12]
+      days: [1,2,3,4,5,6,7,8,9,10,11,12],
+      daysMissed: 0
     },
     {
       name: 'Lilly the Lizard',
-      days: [1,2,3,4,5,6,7,8,9,10,11,12]
+      days: [1,2,3,4,5,6,7,8,9,10,11,12],
+      daysMissed: 0
     },
     {
       name: 'Paulrus the Walrus',
-      days: [1,2,3,4,5,6,7,8,9,10,11,12]
+      days: [1,2,3,4,5,6,7,8,9,10,11,12],
+      daysMissed: 0
     },
     {
       name: 'Gregory the Goat',
-      days: [1,2,3,4,5,6,7,8,9,10,11,12]
+      days: [1,2,3,4,5,6,7,8,9,10,11,12],
+      daysMissed: 0
     },
     {
       name: 'Adam the Anaconda',
-      days: [1,2,3,4,5,6,7,8,9,10,11,12]
+      days: [1,2,3,4,5,6,7,8,9,10,11,12],
+      daysMissed: 0
     },
   ]
 };
@@ -92,20 +97,89 @@ var studentListView = {
       var elem = document.createElement('td');
       elem.textContent = student.name;
 
+      var dayMissed = document.createElement('td');
+      dayMissed.textContent = student.daysMissed;
+      dayMissed.setAttribute("class", "missed-col");
+
       this.studentListElem.appendChild(newTR);
 
+
+
       newTR.appendChild(elem);
+
+      //newTR.appendChild()
       for(var x = 0; x < 12; x++){
         var newTD = document.createElement('td');
         newTD.setAttribute("class", "attend-col");
         newTR.appendChild(newTD);
 
       };
-
+            newTR.appendChild(dayMissed);
     };
     $(".attend-col").append('<input type="checkbox">');
+
+
   }
 };
 
 //make it go
 octopus.init();
+
+
+// Outside MVO
+
+/* STUDENT APPLICATION */
+$(function() {
+    var attendance = JSON.parse(localStorage.attendance),
+        $allMissed = $('tbody .missed-col'),
+        $allCheckboxes = $('tbody input');
+
+    // Count a student's missed days
+    function countMissing() {
+        $allMissed.each(function() {
+            var studentRow = $(this).parent('tr'),
+                dayChecks = $(studentRow).children('td').children('input'),
+                numMissed = 0;
+
+            dayChecks.each(function() {
+                if (!$(this).prop('checked')) {
+                    numMissed++;
+                }
+            });
+
+            $(this).text(numMissed);
+        });
+    }
+
+    // Check boxes, based on attendace records
+    $.each(attendance, function(name, days) {
+        var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
+            dayChecks = $(studentRow).children('.attend-col').children('input');
+
+        dayChecks.each(function(i) {
+            $(this).prop('checked', days[i]);
+        });
+    });
+
+    // When a checkbox is clicked, update localStorage
+    $allCheckboxes.on('click', function() {
+        var studentRows = $('tbody .student'),
+            newAttendance = {};
+
+        studentRows.each(function() {
+            var name = $(this).children('.name-col').text(),
+                $allCheckboxes = $(this).children('td').children('input');
+
+            newAttendance[name] = [];
+
+            $allCheckboxes.each(function() {
+                newAttendance[name].push($(this).prop('checked'));
+            });
+        });
+
+        countMissing();
+        localStorage.attendance = JSON.stringify(newAttendance);
+    });
+
+    countMissing();
+}());
